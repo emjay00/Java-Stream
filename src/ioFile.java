b55e9032f11334fc0a2717d49bb15a452e790b4f
ManuJohn <manusofficial@gmail.com>
2022-06-22 09:47:17 -0400

    test csv html

--- src/ioFile.java
+++ src/ioFile.java
@@ -2,17 +2,37 @@ import java.io.*;
 
 public class ioFile {
     public static void main(String[] args) throws IOException {
-    File f = new File("C:\\Users\\ManuJohn\\Downloads\\text.txt");
+    //File f = new File("C:\\Users\\ManuJohn\\Downloads\\test.txt");
+    File f = new File("C:\\Users\\ManuJohn\\Downloads\\test.csv");
+    //File f = new File("C:\\Users\\ManuJohn\\Downloads\\test.html");
     FileWriter fw= new FileWriter(f);
     BufferedWriter writer = new BufferedWriter(fw);
 
-    writer.write("Fist line");
+    //writer.write("<html> <body> <title> test page </title> <h1> TEST TEST TEST </h1> </body> </html>");
+    /*writer.write("First line");
     writer.newLine();
     writer.write("Second line");
     writer.newLine();
     writer.write("third line");
     writer.newLine();
-    writer.close();
+    writer.write("fourth line");
+    writer.newLine();*/
+
+
+    for (int i=0;i<4;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            int num=(int)(Math.random()*100);
+            writer.write(num+",");
+        }
+        writer.newLine();
+    }
+
+
+        writer.close();
+
+
 
     System.out.println("File created");
 
